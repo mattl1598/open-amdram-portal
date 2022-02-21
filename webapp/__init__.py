3ab2a7499652751ded3399fc13743c31aef51e3c
Matthew Larby <mattl1598@gmail.com>
2022-02-21 14:26:14 +0000

    template layout
    Signed-off-by: Matthew Larby <mattl1598@gmail.com>

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,4 +3,13 @@ import os
 
 app = Flask(__name__)
 
+nav = [
+	("Home", "/",),
+	("Blog", "/blog",),
+	("Past Shows", "/past-shows",),
+	("Auditions", "/auditions",),
+	("About Us", "/about-us",),
+	("Members", "/members",),
+]
+
 from webapp import routes
