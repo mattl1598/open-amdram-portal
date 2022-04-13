33a6d118f27e06cfd40aadefc06c9fb8cb00b197
mattl1598 <mattl1598@gmail.com>
2022-04-13 10:31:02 +0100

    add show members routes
    Signed-off-by: mattl1598 <mattl1598@gmail.com>

--- webapp/members_routes.py
+++ webapp/members_routes.py
@@ -1,10 +1,31 @@
+from corha import corha
 from flask import abort, make_response, redirect, render_template, send_file, url_for, request, session
 from flask_login import login_user, logout_user, current_user, login_required
 from webapp import app, db
-from webapp.models import User
+from webapp.models import Member, User
 
 
 @app.route("/members/dashboard")
 @login_required
 def dashboard():
 	return render_template("members/dashboard.html", css="m_dashboard.css")
+
+
+@app.route("/members/add-show-member", methods=["GET", "POST"])
+def add_show_member():
+	if request.method == "GET":
+		return render_template("members/add_show_member.html", css="m_dashboard.css")
+	elif request.method == "POST":
+		used_ids = [value[0] for value in Member.query.with_entities(Member.id).all()]
+		new_id = corha.rand_string(request.form["firstname"], 16, used_ids)
+
+		new_member = Member(
+			id=new_id,
+			firstname=request.form['firstname'],
+			lastname=request.form['lastname']
+		)
+
+		db.session.add(new_member)
+		db.session.commit()
+
+		return redirect(url_for("add_show_member"))
