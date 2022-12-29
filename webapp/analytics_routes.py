43bdee1e2a74f35c222df3006d50af4183fafe05
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2022-12-29 19:20:13 +0000

    fixing analytics collection for the new blueprint approach
    Signed-off-by: Matthew Larby <mattl1598@gmail.com>

--- webapp/analytics_routes.py
+++ webapp/analytics_routes.py
@@ -7,22 +7,22 @@ from webapp.models import *
 bp = Blueprint("analytics_routes", __name__)
 
 
-@bp.before_request
-def before_request():
+@bp.before_app_request
+def before_app_request():
 	if session.get("session_id") is None:
-		session["session_id"] = corha.rand_string(datetime.datetime.utcnow().isoformat(), 64, [])
+		session["session_id"] = corha.rand_string(datetime.utcnow().isoformat(), 64, [])
 		session.modified = True
 
 
-@bp.after_request
-def after_request(response):
-	db.session.commit()
+@bp.after_app_request
+def after_app_request(response):
 	ignored_endpoints = [
-		"css",
-		"js",
-		"get_photo",
-		"favicon"
+		"routes.css",
+		"routes.js",
+		"photos_routes.get_photo",
+		"routes.favicon"
 	]
+	print(request.endpoint)
 	if request.endpoint not in ignored_endpoints and "None" not in request.url and response.status_code != 302:
 		if current_user.is_authenticated:
 			email = current_user.email
@@ -31,7 +31,7 @@ def after_request(response):
 
 		new_log = AnalyticsLog(
 			id=AnalyticsLog.get_new_id(),
-			date=datetime.datetime.utcnow().isoformat(),
+			date=datetime.utcnow().isoformat(),
 			server=request.root_url,
 			session_id=session["session_id"],
 			session_email=email,
@@ -42,4 +42,7 @@ def after_request(response):
 		)
 		db.session.add(new_log)
 		db.session.commit()
+
+	print("test")
+	db.session.commit()
 	return response
