02a462a772c39c16cc865931f44e26ac4b3205de
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2023-10-21 21:50:35 +0100

    - fixing tests wrt logout
    Signed-off-by: Matthew Larby <37371392+mattl1598@users.noreply.github.com>

--- tests/test_get.py
+++ tests/test_get.py
@@ -5,7 +5,7 @@ from tests.conftest import all_routes, auth_map
 @pytest.mark.all_get
 @pytest.mark.parametrize("route", all_routes)
 def test_all_get(route, test_client, auth):
-	if "logout" not in route:
+	if "logout" not in route or auth == "public":
 		with test_client:
 			response = test_client.get(route, headers={"Referer": '/'})
 			resp_code = response.status_code
