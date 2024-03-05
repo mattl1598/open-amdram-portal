ea2e659cb0a9a9c24bae1cd9309f9b0619a61a8c
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2024-03-05 19:24:51 +0000

    - cleaning up debug logs
    Signed-off-by: Matthew Larby <37371392+mattl1598@users.noreply.github.com>

--- webapp/static/js/img.js
+++ webapp/static/js/img.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 	let image_elements = document.querySelectorAll("img")
 	for (let elem of image_elements) {
-		console.log(elem.id)
 		elem.addEventListener('error', function(){
-			console.log("test")
 			elem.src = elem.src + "?refresh"
 		}, {once: true})
 	}
