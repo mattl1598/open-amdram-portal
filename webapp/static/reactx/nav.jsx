fb0cf483b0bd44c6f753d6d6608e97cc070d6829
Matthew Larby <mattl1598@gmail.com>
2024-05-03 20:47:23 +0100

    - adding members nav to react nav component

--- webapp/static/reactx/nav.jsx
+++ webapp/static/reactx/nav.jsx
@@ -5,42 +5,43 @@ const navItems = [
 	{title: "Auditions", link: "/auditions", class: ""},
 	{title: "About Us", link: "/about-us", class: ""},
 	{title: "Members", link: "/members", class: ""},
-	{title: "Search", link: "/search", class: "search"},
+	{title: "Search", link: "/search", class: "search", icon: "search"},
+]
+
+const memberNavItems = [
+	{title: "Dashboard", link: "/members/dashboard", class: "", icon: "dashboard"},
+	{title: "Shows", link: "/members/shows", class: "", icon: "drama"},
+	{title: "Manage Blog", link: "/members/manage-blog", class: "", icon: "blog_icon"},
+	{title: "Get Subs", link: "/members/get_subs", class: "", icon: "membership"},
+	{title: "Manage Bookings", link: "/members/bookings", class: "", icon: "ticket"},
+	{title: "Admin Tools", link: "/members/admin", class: "", icon: "admin"},
+	{title: "Member Docs", link: "/members/docs", class: "", icon: "note"},
+	{title: "Account Settings", link: "/members/account_settings", class: "", icon: "person"},
+	{title: "Help & Feedback", link: "", class: "", icon: "help"},
+	{title: "Logout", link: "/members/logout", class: "", icon: "logout"},
 ]
 
 const navRoot = ReactDOM.createRoot(document.getElementById('nav'))
-navRoot.render(<Nav navItems={navItems} siteName={siteName}/>)
+navRoot.render(<Nav navItems={navItems} memberNavItems={memberNavItems} siteName={siteName}/>)
 
 function Nav({navItems, siteName}) {
-	const [_, forceRender] = React.useReducer((newVal) => newVal + 1, 0);
-	const [gapWidth, setGapWidth] = React.useState(0);
 	const navList = []
+	const memberNavList = []
 	const gapRef = React.useRef()
 	const [mobileNav, setMobileNav] = React.useState(false)
 	const [expanded, setExpanded] = React.useState(false)
 
 	for (let i=0; i<navItems.length; i++) {
 		let is_active = navItems[i].link.split("/")[1] === window.location.pathname.split("/")[1];
-		if (navItems[i].class === "search") {
-			navList.push(
-				<a href={navItems[i].link}
-				   className={is_active ? `${navItems[i].class} active` : `${navItems[i].class}`}
-				   style={{width: `calc(100dvw - ${mobileNav ? document.body.offsetWidth - document.body.clientWidth : 0}px)`}}
-				>
-					<Icon icon={"search"}></Icon>
-					<h3 class={"mobile"}>{navItems[i].title}</h3>
-				</a>
-			)
-		} else {
-			navList.push(
-				<a href={navItems[i].link}
-				   className={is_active? `${navItems[i].class} active`: `${navItems[i].class}`}
-			        style={{width: `calc(100dvw - ${mobileNav ? document.body.offsetWidth-document.body.clientWidth : 0}px)`}}
-				>
-					<h3>{navItems[i].title}</h3>
-				</a>
-			)
-		}
+		navList.push(
+			<NavItem item={navItems[i]} is_active={is_active} mobileNav={mobileNav}></NavItem>
+		)
+	}
+	for (let i=0; i<memberNavItems.length; i++) {
+		let is_active = memberNavItems[i].link.split("/")[1] === window.location.pathname.split("/")[1];
+		memberNavList.push(
+			<NavItem item={memberNavItems[i]} is_active={is_active} mobileNav={mobileNav}></NavItem>
+		)
 	}
 
 	React.useEffect(() => {
@@ -60,7 +61,6 @@ function Nav({navItems, siteName}) {
 		setExpanded(!expanded)
 	}
 
-// TODO: load correct site title
 	return (
 		<div id={"react-nav"} className={"react-nav"}>
 			<div className={"title"}>
@@ -88,6 +88,33 @@ function Nav({navItems, siteName}) {
 			<div className={`mobile-nav ${mobileNav && expanded ? "show" : "hide"}`}>
 				{navList}
 			</div>
+			<div className={"member-nav"}>
+				{memberNavList}
+			</div>
 		</div>
 	)
+}
+
+function NavItem({item, is_active, mobileNav}) {
+	if (item.icon !== undefined) {
+		return (
+			<a href={item.link}
+			   className={is_active ? `${item.class} active` : `${item.class}`}
+			   style={{width: `calc(100dvw - ${mobileNav ? document.body.offsetWidth - document.body.clientWidth : 0}px)`}}
+			>
+				<Icon icon={item.icon}></Icon>
+				<h3 className={"mobile"}>{item.title}</h3>
+			</a>
+		)
+	} else {
+		return (
+			<a href={item.link}
+			   className={is_active ? `${item.class} active` : `${item.class}`}
+			   style={{width: `calc(100dvw - ${mobileNav ? document.body.offsetWidth - document.body.clientWidth : 0}px)`}}
+			>
+				<h3>{item.title}</h3>
+			</a>
+		)
+	}
+
 }
